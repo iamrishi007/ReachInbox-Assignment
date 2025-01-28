b281a3060068deb851e9cd56083e4fb8d4d2b28d
Rushikesh Gaware <rushigaware1020@gmail.com>
2025-01-28 18:10:24 +0530

    .ui update

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { GoogleOAuthProvider } from '@react-oauth/google';
+import { ChakraProvider } from '@chakra-ui/react'
 
 
 createRoot(document.getElementById('root')).render(
-  <GoogleOAuthProvider clientId="443924821698-dd7p6srlcscbb6rq1c81ubjhp2kqipbn.apps.googleusercontent.com">
+ <ChakraProvider>
+   <GoogleOAuthProvider clientId="443924821698-dd7p6srlcscbb6rq1c81ubjhp2kqipbn.apps.googleusercontent.com">
     <App />
   </GoogleOAuthProvider>,
+ </ChakraProvider>
 )
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,32 +1,38 @@
-
 import { GoogleLogin } from '@react-oauth/google';
+import { Button, Box, Text } from '@chakra-ui/react';
 
 function Homepage() {
-     <h1>React Login</h1>
+    return (
+        <>
+            <Box textAlign="center" p={5} mb={5}>
+                <Text fontSize="2xl" fontWeight="bold">React Login</Text>
+            </Box>
 
-     
+            <Box
+                p={6}
+                border="1px solid #ccc"
+                borderRadius="md"
+                boxShadow="md"
+                mb={5}
+                textAlign="center"
+            >
+                <Text fontSize="xl" mb={4}>Create new account</Text>
+                <GoogleLogin
+                    onSuccess={credentialResponse => {
+                        console.log(credentialResponse);
+                    }}
+                    onError={() => {
+                        console.log('Login Failed');
+                    }}
+                />
+            </Box>
 
-     return (
-          <>
-
-
-               <div style={{ border: "1px solid red" }}>
-                    <GoogleLogin
-                         onSuccess={credentialResponse => {
-                              console.log(credentialResponse);
-                         }}
-                         onError={() => {
-                              console.log('Login Failed');
-                         }}
-                    />
-               </div>
-               <div>
-
-               
-               </div>
-
-          </>
-     )
+            <Box textAlign="center">
+                <Text fontSize="lg" mb={2}>Already have an account?</Text>
+                <Button colorScheme="blue">Sign In</Button>
+            </Box>
+        </>
+    );
 }
 
-export default Homepage
+export default Homepage;
